900539e755a6e774c49ce4dbdbc2005b62c0ec0d
alimertcetin <mrtmert54@gmail.com>
2024-07-01 03:16:55 +0000

    feat(main.cpp): Test HashSet

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
+#include <typeinfo>
 #include "List.hpp"
+#include "HashSet.hpp"
+
+template<typename T>
+void Populate(T& list)
+{
+  std::cout << "add: " << typeid(list).name() << "\n";
+  
+  for (int i = 0; i < 100; ++i) {
+    list.Add(i);
+  }
+}
+
+template<typename T>
+void Log(T& list)
+{
+  
+  for (auto it = list.begin(); it != list.end(); it++)
+    {
+      std::cout << *it << "\n";
+    }
+}
+
+template<typename T>
+void Clear(T& list)
+{
+  std::cout << "clear: " << typeid(list).name() << "\n";
+
+  list.Clear();
+  
+  std::cout << list.count() << "\n";
+}
+
+template<typename T>
+void Test(T& list)
+{
+  Populate(list);
+  Log(list);
+  Clear(list);
+}
 
 int main(int argc, char* argv[])
 {
-  List<int> l;
-  for (int i = 0; i < 100; ++i) {
-    l.Add(i);
-  }
-
-  for (int i = 0; i < l.count(); ++i) {
-    std::cout << l[i] << "\n";
-
-  }
-
-  l.Clear();
-  std::cout << l.count() << "\n";
-
+  //List<int> list;
+  HashSet<int> hashSet;
+  //Test(list);
+  Test(hashSet);
 }
